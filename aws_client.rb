0d63cffb9a9e6bfa7223486a5e5aa619edd993be
Hank Beaver <hbeaver@blinqmedia.com>
2012-02-17 17:36:15 -0500

    Added rails

--- aws_client.rb
+++ aws_client.rb
@@ -1,2 +1,11 @@
-config = YAML.load_file("cloud_config.yml")
+require 'fog'
 
+class AwsClient
+
+
+end
+
+
+config = YAML.load_file("cloud_config.yml")
+@compute = Fog::Compute.new({:provider => 'AWS', :aws_access_key_id => config['aws']['access_key'], :aws_secret_access_key => config['aws']['secret_access_key']})
+@storage = Fog::Storage.new({:provider => 'AWS', :aws_access_key_id => config['aws']['access_key'], :aws_secret_access_key => config['aws']['secret_access_key']})
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MinecraftEc2::Application.config.secret_token = 'cd8f441d8a90bf147268bfd0e542c58d8a58b1818c58c14d018d29cb17f1284612c5ae219792dbc8f6b8a3890cae628b46c7f919e00b0566358aa58fd5ed25b2'
